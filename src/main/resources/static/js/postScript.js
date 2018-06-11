a7600216669d1353623eee6ca894fff6f574d4ee
Adela Vasilache <adela.vasilache@pitechnologies.ro>
2018-06-11 18:10:16 +0300

    add rating system-front

--- src/main/resources/static/js/postScript.js
+++ src/main/resources/static/js/postScript.js
@@ -1,26 +1,66 @@
 $(document).ready(function () {
-    ratePost();
+    colorRatePost();
 });
 
 var rateUrl = "/post/rate";
 var latestUrl = "/post/latest/9";
 
-function ratePost() {
-    $('.btn-rate').click(function () {
+
+function colorRatePost() {
+    var animationTime = 100;
+    var colours = ["bd2c33", "e49420", "ecdb00", "3bad54", "1b7db9"];
+
+    var ratingInfobox = $("<div />")
+        .attr("id", "ratinginfo")
+        .insertAfter($("#rating"));
+
+    var colourizeRatings = function (nrOfRatings) {
+        $('#rating li').each(function () {
+            if ($(this).index() <= nrOfRatings) {
+                $(this).stop().animate({backgroundColor: "#" + colours[nrOfRatings]}, animationTime);
+            }
+        });
+    };
+
+    $("#rating li").hover(function() {
+
+        ratingInfobox
+            .empty()
+            .stop()
+            .animate({ opacity : 1 }, animationTime);
+
+        $("<p />")
+            .html($(this).html())
+            .appendTo(ratingInfobox);
+
+        colourizeRatings($(this).index());
+    }, function() {
+
+        ratingInfobox
+            .stop()
+            .animate({ opacity : 0 }, animationTime);
+
+        $("#rating li").stop().animate({ backgroundColor : "#333" } , animationTime);
+    });
+    $("#rating li").click(function(e) {
+        e.preventDefault();
         var dataObject = JSON.stringify({
-            postId: $(this).closest('input').find('.post-id'),
-            // postId: $('#postId').val(),
-            grade: $(this).val()
+            postId : $(this).parent().attr('data-id'),
+            grade: $(this).index() + 1
         });
         $.ajax({
             contentType: "application/json; charset=utf-8",
             url: rateUrl,
             type: "POST",
             data: dataObject,
-            success: function(data){
-               location.reload();
-            }
+            success:window.location.reload()
         })
-    })
+    });
 
+    // $("#rating li")
+    //     .on("mouseleave", function() {
+    //     $('#ratinginfo').remove();
+    // })
 }
+
+
